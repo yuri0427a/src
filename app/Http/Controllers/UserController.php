eac5d7071c3c32207b7c47ef1aeb6c23b472fddb
板谷優里 <itayayuri@yuri-mac.local>
2021-07-19 17:28:10 +0900

    [Modified]日本語表記

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+  {
+      // DBよりBookテーブルの値を全て取得
+      $books = Book::all();
+
+      // 取得した値をビュー「book/index」に渡す
+      return view('book/index', compact('books'));
+  }
+
 }
