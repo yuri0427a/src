39f821281a6481512d6738460d6887640c9a2987
板谷優里 <itayayuri@yuri-mac.local>
2021-07-21 11:28:24 +0900

    [Add]ユーザー/詳細ページ ルート

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 // UserController
 
-Route::get('/user/{id}', [App\Http\Controllers\UserController::class,  "show"])->name("users.show");
+Route::get('/users/{id}', [App\Http\Controllers\UserController::class,  "show"])->name("users.show");
 
 // QuestionController
 
