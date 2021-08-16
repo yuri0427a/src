9e24304555879a4f678a6387931d755b7cc066cb
yuri0427a <yuri0427ita@gmail.com>
2021-08-16 14:30:57 +0900

    [Add]nav_mypage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">
+                                    <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
                                         {{ __('MyPage') }}
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
