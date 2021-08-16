d2f2d8ee48bb84467aa2e023c6529c7aa694418c
yuri0427a <yuri0427ita@gmail.com>
2021-08-16 13:57:19 +0900

    [Add]マイページ 自分が投稿したお題一覧

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,4 +10,8 @@
 {{ $user->email }}
 {{ $user->introduction }}
 <a href="{{ route('users.edit', $user->id) }}" ><button class='btn btn-info'>編集</button></a>
+@foreach ($questions as $question)
+<a href="{{ route('questions.show', $question->id)}}">
+        {{ $question->title }}
+ @endforeach
 @endsection
