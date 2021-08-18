a31e6b9232afdd3988bc8273f39b20c1fb1b475f
yuri0427a <yuri0427ita@gmail.com>
2021-08-18 17:04:03 +0900

    [Add]コード整形

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,16 +6,13 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-
-                </div>
+               </div>
             </div>
         </div>
     </div>
