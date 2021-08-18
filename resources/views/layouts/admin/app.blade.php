cb38be3237678c147482be059ff7df9f9634654e
yuri0427a <yuri0427ita@gmail.com>
2021-08-18 17:04:40 +0900

    [Add]コード整形

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -33,10 +33,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
                     </ul>
-
-                    <!-- Right Side Of Navbar -->
+                   <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @unless (Auth::guard('admin')->check())
